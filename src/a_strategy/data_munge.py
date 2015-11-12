daf2c17b7b2576f57fbb4b7c62fc1f319f29c956
Xiang Li <xlees58@gmail.com>
2015-11-12 21:53:20 +0800

    实现检测涨停点算法。

--- src/a_strategy/data_munge.py
+++ src/a_strategy/data_munge.py
@@ -6,6 +6,7 @@ Created on 2015��11��12��
 '''
 
 import tushare as ts
+import numpy as np
 from datetime import datetime
 
 
@@ -16,10 +17,30 @@ def check_if_reach_limit(code, date=datetime.now().strftime("%Y-%m-%d")):
     if kdata.ix[0]['p_change'] < 9.9:
         return False
     
-    stock = ts.get_tick_data(code,date)
-    price = stock['price']
-    # 查找涨停区间
+    stock = ts.get_tick_data(code,date)[['time','price']]
+    indx, = np.where(np.diff(list(reversed(stock['price'])))==0)
     
+#     print "length: ", len(indx)
+    
+    arr = []
+    beg = 0
+    cur = indx[0]-1
+    for i,val in enumerate(indx):
+        if cur+1 == val:
+            cur += 1
+        else:
+            if i > (beg+1):
+                arr.append((indx[beg], indx[i-1]))
+            beg = i
+            cur = val
+    arr.append((indx[beg],cur))
+    mindx = np.argmax([x[1]-x[0] for x in arr])
+        
+    print "length of arr: ", len(arr)
+        
+    p = arr[mindx][0]
+    print 'starting index: ', p
+    print 'starting time: ', stock.ix[stock.shape[0]-p-1]['time']
     
     return True
 
@@ -27,12 +48,12 @@ if __name__ == '__main__':
     print "tushare"
     tod = datetime.now().strftime("%Y-%m-%d")
     
-    df = ts.get_realtime_quotes('300212')   # 实时分笔
+    df = ts.get_realtime_quotes('300480')   # 实时分笔
     curdata = df.iloc[0]
     print len(curdata)
     print curdata[['name','price','ask','bid']]
     
-    print check_if_reach_limit('300212', '2015-11-09')
+    print check_if_reach_limit('300382', '2015-11-12')
     
 #     fc = ts.forecast_data(2015,2)
 #     print fc.shape
