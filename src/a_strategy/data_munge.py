1761e6cda45eb2c7ee11f3bc1a28ab4fda9e8378
Xiang Li <xlees58@gmail.com>
2015-12-18 17:55:38 +0800

    获取tick数据。

--- src/a_strategy/data_munge.py
+++ src/a_strategy/data_munge.py
@@ -10,51 +10,65 @@ import pandas as pd
 from datetime import datetime
 import arrow
 import random
-import code
+import time
 
 
 
-def download_tick_data(days=3):
-    stocks = random.sample(ts.get_stock_basics().index, 5)
-#     stocks = ts.get_stock_basics().index
-    _dates = ts.get_h_data('000001', index=True).index[:days]
-    dates = [x.to_datetime().strftime("%Y-%m-%d") for x in _dates]
+def download_tick_data(cdate=arrow.now().format('YYYY-MM-DD'), days=3):
+	tod = arrow.get(cdate)
+	_end = tod.format('YYYY-MM-DD')
+	_start = tod.replace(days=-days).format('YYYY-MM-DD')
+	print 'start=%s, end=%s' % (_start,_end)
+	
+#	stocks = random.sample(ts.get_stock_basics().index, 5)
+	stocks = ts.get_stock_basics().index
+	_dates = ts.get_h_data('000001', start=_start, end=_end, index=True).index[:days]
+	dates = [x.to_datetime().strftime("%Y-%m-%d") for x in _dates]
+	
+	print "%d stocks retrieved." % (len(stocks))
+	time.sleep(5.0)
     
-    def tick(tstamp):
-        date = tstamp.split(" ")[0]
-        tick_data = ts.get_tick_data(code,date=date,retry_count=10,pause=2)
-        nrows = tick_data.shape[0]
-        tick_data['date'] = pd.Series([date]*nrows)
-        
-        return tick_data
-    
-    def trans(ttype):
-        _ttype = ttype.decode('utf-8')
-        if _ttype == u'买盘':
-            return 2
-        elif _ttype == u'卖盘':
-            return 1
-        else:
-            return 0
-    
-    cols = ['date','code','time','type','price','volume','amount']
-    for code in stocks:
-        try:
-            df = map(tick, dates)
-            print 'code=', code, '. list shape:\n', df[0].head(2)
-            
-            print 'saving stock data...'
-            result = pd.concat(df)
-            result['code'] = pd.Series([code]*result.shape[0])
-            
-            # change type to digit
-            result['type'] = map(trans, result['type'])
-            
-            result[cols].to_csv("stocks/%s.csv" % (code), mode='w',header=False,index=False)
-        except Exception as e:
-            print e
-    
-    print 'stocks size: ',len(stocks), "head days: ", dates[:1]
+	def tick(tstamp):
+	    date = tstamp.split(" ")[0]
+	    tick_data = ts.get_tick_data(code,date=date,retry_count=10,pause=5)
+	    nrows = tick_data.shape[0]
+	    tick_data['date'] = pd.Series([date]*nrows)
+	    
+	    return tick_data
+	
+	def trans(ttype):
+	    _ttype = ttype.decode('utf-8')
+	    if _ttype == u'买盘':
+	        return 2
+	    elif _ttype == u'卖盘':
+	        return 1
+	    else:
+	        return 0
+	
+	cols = ['date','code','time','type','price','volume','amount']
+	n_avail = 0
+	for code in stocks:
+	    try:
+	        df = map(tick, dates)
+	        print 'code=', code, '. list shape:\n', df[0].head(2)
+	        
+	        print 'saving stock data...'
+	        result = pd.concat(df)
+	        result['code'] = pd.Series([code]*result.shape[0])
+	        
+	        # change type to digit
+	        result['type'] = map(trans, result['type'])
+	        
+	        result[cols].to_csv("stocks/%s.csv" % (code), mode='w',header=False,index=False)
+	        
+	        n_avail += 1
+	    except Exception as e:
+	        print "stock %s failure." % (code)
+	        
+	    print "%s finished." % (code)
+	    time.sleep(0.5)
+	
+	print "\n%d stocks, %d avail, %d unavailable." % (len(stocks), n_avail, len(stocks)-n_avail)
 
 def check_if_reach_limit(code, date=datetime.now().strftime("%Y-%m-%d")):
     """
@@ -94,7 +108,7 @@ if __name__ == '__main__':
     print "tushare"
     tod = datetime.now().strftime("%Y-%m-%d")
     
-    download_tick_data()
+    download_tick_data(days=5)
     
 #     df = ts.get_realtime_quotes('300480')   # 实时分笔
 #     curdata = df.iloc[0]
